df6d5f65fb55da9b3a3f77bdc8b915e2c426ef58
Shakoh <70974710+Shakohh@users.noreply.github.com>
2023-08-07 20:07:53 +0200

    Nix support

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    crane.url = "github:ipetkov/crane";
+    crane.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, crane, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        craneLib = crane.lib.${system};
+      in
+    {
+      packages.default = craneLib.buildPackage {
+        src = craneLib.cleanCargoSource (craneLib.path ./.);
+          doCheck = true;
+          #buildInputs = [];
+          #nativeBuildInputs = [];
+      };
+    });
+}
