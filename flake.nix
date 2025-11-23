bdb464c69de2ae2045ae40e46059a1328ecb35c2
doprz <52579214+doprz@users.noreply.github.com>
2025-11-22 21:20:38 -0600

    chore: remove extra comments

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "dipc";
+  description = "Convert your favorite images and wallpapers with your favorite color palettes/themes";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -41,29 +41,22 @@
           ];
         };
 
-        # Common arguments can be set here to avoid repeating them later
         commonArgs = {
           inherit src;
           strictDeps = true;
 
           buildInputs = [
-            # Add additional build inputs here
           ]
           ++ lib.optionals pkgs.stdenv.isDarwin [
-            # Additional darwin specific inputs can be set here
             pkgs.libiconv
           ];
-
-          # Additional environment variables can be set directly
-          # MY_CUSTOM_VAR = "some value";
         };
 
         # Build *just* the cargo dependencies, so we can reuse
         # all of that work (e.g. via cachix) when running in CI
         cargoArtifacts = craneLib.buildDepsOnly commonArgs;
 
-        # Build the actual crate itself, reusing the dependency
-        # artifacts from above.
+        # Build the actual crate itself, reusing the dependency artifacts from above.
         dipc = craneLib.buildPackage (commonArgs // { inherit cargoArtifacts; });
       in
       {
@@ -89,23 +82,16 @@
             commonArgs
             // {
               inherit cargoArtifacts;
-              # This can be commented out or tweaked as necessary, e.g. set to
-              # `--deny rustdoc::broken-intra-doc-links` to only enforce that lint
               env.RUSTDOCFLAGS = "--deny warnings";
             }
           );
 
-          # Check formatting
           dipc-fmt = craneLib.cargoFmt { inherit src; };
-
-          # Audit dependencies
           dipc-audit = craneLib.cargoAudit { inherit src advisory-db; };
-
-          # Audit licenses
           dipc-deny = craneLib.cargoDeny { inherit src; };
 
           # Run tests with cargo-nextest
-          # Consider setting `doCheck = false` on `my-crate` if you do not want
+          # Consider setting `doCheck = false` on `dipc` if you do not want
           # the tests to run twice
           dipc-nextest = craneLib.cargoNextest (
             commonArgs
@@ -120,6 +106,7 @@
 
         packages = {
           default = dipc;
+          inherit dipc;
         };
 
         apps.default = flake-utils.lib.mkApp { drv = dipc; };
@@ -129,9 +116,6 @@
         devShells.default = craneLib.devShell {
           # Inherit inputs from checks.
           checks = self.checks.${system};
-
-          # Additional dev-shell environment variables can be set directly
-          # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
 
           # Extra inputs can be added here; cargo and rustc are provided by default.
           packages = with pkgs; [ rust-analyzer ];
