a07b6ae3e42c61a5b283588b62051337eb7ab593
doprz <52579214+doprz@users.noreply.github.com>
2025-12-20 23:08:18 -0600

    feat: add non-blocking poll

--- src/tui.rs
+++ src/tui.rs
@@ -7,15 +7,21 @@ use ratatui::{
     prelude::CrosstermBackend,
     Terminal,
 };
-use std::io::{stdout, Result};
+use std::{
+    io::{stdout, Result},
+    time::{Duration, Instant},
+};
 
-use crate::tui::{app::App, ui::ui};
+use crate::tui::{
+    app::{App, TuiConfig},
+    ui::ui,
+};
 
 mod app;
 mod ui;
 mod utils;
 
-pub fn run() -> Result<Option<app::TuiConfig>> {
+pub fn run() -> Result<Option<TuiConfig>> {
     // Pre-run
     enable_raw_mode()?;
     let mut stdout = stdout();
@@ -26,14 +32,25 @@ pub fn run() -> Result<Option<app::TuiConfig>> {
 
     let mut app = App::new();
 
+    let tick_rate = Duration::from_millis(250);
+    let mut last_tick = Instant::now();
+
     // UI Loop
     loop {
         terminal.draw(|f| ui(f, &mut app))?;
 
-        if let Event::Key(key) = event::read()? {
-            if key.kind == KeyEventKind::Press {
-                app.handle_key(key.code, key.modifiers);
+        // Non-blocking poll
+        let timeout = tick_rate.saturating_sub(last_tick.elapsed());
+        if event::poll(timeout)? {
+            if let Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press {
+                    app.handle_key(key.code, key.modifiers);
+                }
             }
+        }
+
+        if last_tick.elapsed() >= tick_rate {
+            last_tick = Instant::now();
         }
 
         if app.should_quit || app.should_run {
